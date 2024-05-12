3eb5ee54f05105628b2899ab556af12022ca515d
Aleksandr22822 <alek5andr.bubnov@yandex.ru>
2024-05-12 15:52:33 +0300

    Добавьте файлы проекта.

---
+++ Задание 12.1.5/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Task_12._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>() { new User("Александр", "arbuka77", true), new User("Алексей", "refos228", false) };
+            foreach (var user in users)
+            {
+                
+                if (user.IsPremium == true)
+                {
+                    user.GetUpdate();
+                    Console.WriteLine($"Здравствуйте {user.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"Здравствуйте {user.Name}");
+                    Advertisement.ShowAds();
+                }
+            }
+        
+        }
+    }
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+        public User(string Name, string Login, bool IsPremium)
+        {
+            this.Name = Name;
+            this.Login = Login;
+            this.IsPremium = IsPremium;
+        }
+
+      public void GetUpdate()
+        {
+            Console.WriteLine("VIP пользователь, реклама отклбчена");
+        }
+
+    }
+
+    internal static class Advertisement
+    {
+        public static void ShowAds()
+        {
+            Console.WriteLine("Авторизован пользователь, не имеющий VIP статуса");
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
